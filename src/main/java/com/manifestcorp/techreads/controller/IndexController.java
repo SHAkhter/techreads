385d745f6b26a5fd3873ae69f6e67da207d798d8
Saife Akhter <sakhter@manifestcorp.com>
2018-02-05 16:27:50 -0500

    Added an add functionality and default list of books to the web application.

--- src/main/java/com/manifestcorp/techreads/controller/IndexController.java
+++ src/main/java/com/manifestcorp/techreads/controller/IndexController.java
@@ -2,12 +2,14 @@ package com.manifestcorp.techreads.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
+@RequestMapping("/")
 public class IndexController {
 
 	@RequestMapping("/")
-	public String home() {
-		return "Hello World!";
+	public RedirectView index(){
+		return new RedirectView("books");
 	}
 }
